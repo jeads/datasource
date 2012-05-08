21da4053d60137ce5842c2db9b1163fb2ebdf99b
Carl Meyer <carl@oddbird.net>
2012-05-08 17:51:15 -0600

    Better dependency handling in setup.py.

--- setup.py
+++ setup.py
@@ -1,31 +1,27 @@
 import os, stat
-from distutils.core import setup
-
+kwargs = {}
 try:
-   import MySQLdb
+    from setuptools import setup
+    kwargs["install_requires"] = ["MySQL-python"]
 except ImportError:
-   raise Exception('datasource requires the python module MySQLdb (http://sourceforge.net/projects/mysql-python/')
+    from distutils.core import setup
 
 def read(fname):
    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
-#Set datas_sources.json to be readable by everyone
-dirName = os.path.dirname(__file__)
-fileName = '.' + dirName + '/datasource/data_sources.json'
-os.chmod(fileName, stat.S_IREAD|stat.S_IWRITE|stat.S_IRGRP|stat.S_IWGRP|stat.S_IROTH)
-
-setup(name='datasource',
-      version='0.5',
-      description='Data Source Encapsulation',
-      license='MPL',
-      keywords = "data SQL MySQL", 
-      author='Jonathan Eads (Jeads)',
-      packages=['datasource', 'datasource.bases', 'datasource.hubs', 'datasource.t'],
-      long_description=read('README'),
-      package_data={'datasource':['procs/mysql_procs/*.json',
+setup(
+    name='datasource',
+    version='0.5',
+    description='Data Source Encapsulation',
+    license='MPL',
+    keywords = "data SQL MySQL", 
+    author='Jonathan Eads (Jeads)',
+    packages=['datasource', 'datasource.bases', 'datasource.hubs', 'datasource.t'],
+    long_description=read('README'),
+    package_data={'datasource':['procs/mysql_procs/*.json',
                                   't/*.txt',
                                   '*.json',
                                   '*.txt',
-                                  'README'] }
-                                  
-      )
+                                  'README'] },
+    **kwargs
+    )
