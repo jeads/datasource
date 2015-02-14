d014b8ce8d74bae568c896a4ccfabde61742b3e2
Ed Morley <emorley@mozilla.com>
2015-02-14 00:54:58 +0000

    Bump the version to 0.6

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def read(fname):
 
 setup(
     name='datasource',
-    version='0.5',
+    version='0.6',
     description='Data Source Encapsulation',
     license='MPL',
     keywords = "data SQL MySQL", 
